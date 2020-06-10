f95a48919ca3cc8f9ae58b15414545fc75f6ceed
cyan <1227233860@qq.com>
2020-06-11 02:28:08 +0800

    complete menu module on index page

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,77 @@
 <template>
 	<view class="content">
-		<view class="header">
-			<image class="banner-bg" src="../../static/images/banner-bg.jpg"></image>
-			<view class="header-container">
-				<view class="search-bar">
-					<icon class="iconfont icon-category"></icon>
-					<view class="input-area">
-						<image class="jd-logo" src="../../static/images/jd-logo.png"></image>
-						<u-line direction="col"/>
-						<u-icon class="icon-search" name="search"></u-icon>
-						<u-input placeholder="怡宝矿泉水" :customStyle="{fontSize: '20rpx'}" placeholderStyle="color: #aaa"></u-input>
-					</view>
-					<text class="login-btn">登录</text>
+		<view class="header-container">
+			<view class="search-bar">
+				<icon class="iconfont icon-category"></icon>
+				<view class="input-area">
+					<image class="jd-logo" src="../../static/images/jd-logo.png"></image>
+					<u-line direction="col" />
+					<u-icon class="icon-search" name="search"></u-icon>
+					<u-input placeholder="怡宝矿泉水" :customStyle="{fontSize: '20rpx'}" placeholderStyle="color: #aaa"></u-input>
 				</view>
-				<view class="swiper-container">
-					<u-swiper :list="swiperDataSource"></u-swiper>
+				<text class="login-btn">登录</text>
+			</view>
+			<view class="swiper-container">
+				<u-swiper :list="swiperDataSource"></u-swiper>
+			</view>
+		</view>
+		<view>
+			<view class="view-pager">
+				<view>
+					<image src="../../static/images/index-menu1.png"></image>
+					<text>京东超市</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu2.png"></image>
+					<text>数码电器</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu3.png"></image>
+					<text>京东服饰</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu4.png"></image>
+					<text>京东生鲜</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu5.png"></image>
+					<text>京东到家</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu6.png"></image>
+					<text>充值缴费</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu7.png"></image>
+					<text>9.9拼购</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu8.png"></image>
+					<text>领券</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu9.png"></image>
+					<text>借钱</text>
+				</view>
+				<view>
+					<image src="../../static/images/index-menu10.png"></image>
+					<text>PLUS会员</text>
+				</view>
+			</view>
+		</view>
+		<view class="seckill">
+			<image class="index-activity" src="../../static/images/index-activity.png"></image>
+			<view class="seckill-container">
+				<view class="seckill-title">
+					<view>
+						<text>京东秒杀</text>
+						<view class="countdown">
+							<text>18点场</text>
+							<text>00:00:00</text>
+						</view>
+					</view>
+					<text class="more-seckill">更多秒杀</text>
+					<image class="more-seckill-icon" src="../../static/images/more-seckill.png"></image>
 				</view>
 			</view>
 		</view>
@@ -43,54 +100,6 @@
 	}
 </script>
 
-<style>
-	.content {
-		background-color: #f6f6f6;
-	}
-	.header {
-		position: relative;
-	}
-	.banner-bg {
-		width: 100%;
-	}
-	.header-container {
-		position: absolute;
-		width: 100%;
-		top: 0;
-	}
-	.search-bar {
-		display: flex;
-		align-items: center;
-	}
-	.input-area {
-		flex-grow: 1;
-		background-color: white;
-		border-radius: 10000rpx;
-		height: 48rpx;
-		display: flex;
-		align-items: center;
-		padding: 4rpx 24rpx;
-	}
-	.jd-logo {
-		height: 26rpx;
-		width: 32rpx;
-	}
-	u-line {
-		height: 80%;
-		padding: 0 16rpx;
-	}
-	.icon-search {
-		color: #ccc;
-		padding-right: 12rpx;
-	}
-	.icon-category {
-		font-size: 48rpx;
-	}
-	.login-btn, .icon-category {
-		color: white;
-		padding: 22rpx;
-	}
-	.swiper-container {
-		margin: 0 22rpx;
-	}
+<style lang="scss">
+	@import 'index.scss';
 </style>
